7bafe9227faed3e89cc5db9b2cf4fc72d6236ed5
Mohammad Sameer <sammusmarty563@gmail.com>
2025-02-25 18:42:51 +0530

    Update songs.js

--- songs.js
+++ songs.js
@@ -4,7 +4,6 @@ let songsObject = {
     singer: `Shubh`,
     songCover: `https://i.scdn.co/image/ab67616d0000b2731a8c4618eda885a406958dd0`,
     songAudio: `assets/songs/cheques.mp3`,
-    songImageALTText: `Cover image for the song Cheques by Shubh`,
     themeColor: `#6f7974,black`,
   },
   weRollin: {
@@ -12,7 +11,6 @@ let songsObject = {
     singer: `Shubh`,
     songCover: `https://a10.gaanacdn.com/gn_img/albums/MmqK5EKwRO/qK5pE8Z0bw/size_l.jpg`,
     songAudio: `assets/songs/weRollin.mp3`,
-    songImageALTText: `Cover image for the song We-Rollin by Shubh`,
     themeColor: `#686868,black`,
   },
   elevated: {
@@ -20,7 +18,6 @@ let songsObject = {
     singer: `Shubh`,
     songCover: `https://i.scdn.co/image/ab67616d0000b27303712fdb8fc3156aee9f85af`,
     songAudio: `assets/songs/elevated.mp3`,
-    songImageALTText: `Cover image for the song Elevated by Shubh`,
     themeColor: `#c80000,black`,
   },
   stillRollin: {
@@ -28,7 +25,6 @@ let songsObject = {
     singer: `Shubh`,
     songCover: `https://i.scdn.co/image/ab67616d0000b2731a8c4618eda885a406958dd0`,
     songAudio: `assets/songs/stillRollin.mp3`,
-    songImageALTText: `Cover image for the song Still-Rollin by Shubh`,
     themeColor: `#6f7974,black`,
   },
   ishq: {
@@ -37,7 +33,6 @@ let songsObject = {
     // songCover: `https://c.saavncdn.com/206/Ishq-From-Lost-Found-Hindi-2024-20240316114357-500x500.jpg`,
     songCover: `https://i.scdn.co/image/ab67616d00001e020de04fd2c7c3bdc0a1d765f5`,
     songAudio: `assets/songs/ishq.mp3`,
-    songImageALTText: `Cover image for the song Ishq by Faheem Abdullah`,
     themeColor: `#535353,black`,
   },
   besabriyaan: {
@@ -45,7 +40,6 @@ let songsObject = {
     singer: `Armaan Malik`,
     songCover: `https://pagalnew.com/coverimages/Besabriyaan-M.S.-Dhoni---The-Untold-Story-500-500.jpg`,
     songAudio: `assets/songs/besabriyaan.mp3`,
-    songImageALTText: `Cover image for the song Besabriyaan by Armaan Malik`,
     themeColor: `#484850,black`,
   },
   pehlaPyaar: {
@@ -53,7 +47,6 @@ let songsObject = {
     singer: `Armaan Malik`,
     songCover: `https://pagalfree.com/images/128Pehla%20Pyaar%20-%20Kabir%20Singh%20128%20Kbps.jpg`,
     songAudio: `assets/songs/pehlaPyaar.mp3`,
-    songImageALTText: `Cover image for the song Pehla-Pyaar by Armaan Malik`,
     themeColor: `#c81820,black`,
   },
   kyaMujhePyaarHai: {
@@ -61,7 +54,6 @@ let songsObject = {
     singer: `Pritam`,
     songCover: `https://pagalnew.com/coverimages/Kya-Mujhe-Pyaar-Hai-Woh-Lamhe-500-500.jpg`,
     songAudio: `assets/songs/kyaMujhePyaarHai.mp3`,
-    songImageALTText: `Cover image for the song Kya Mujhe Pyaar Hai by Pritam and KK`,
     themeColor: `#a46a3f,black`,
   },
   meraNaamTu: {
@@ -69,7 +61,6 @@ let songsObject = {
     singer: `Abhay Jodhpukar`,
     songCover: `https://c.saavncdn.com/586/Zero-Hindi-2018-20200902134005-500x500.jpg`,
     songAudio: `assets/songs/meraNaamTu.mp3`,
-    songImageALTText: `Cover image for the song Mera Naam Tu by Abhay Jodhpukar`,
     themeColor: `#580808,#580808`,
   },
   khaab: {
@@ -77,7 +68,6 @@ let songsObject = {
     singer: `Akhil`,
     songCover: `https://pagalnew.com/coverimages/Khaab-Akhil-500-500.jpg`,
     songAudio: `assets/songs/khaab.mp3`,
-    songImageALTText: `Cover image for the song Khaab by Akhil`,
     themeColor: `#388090,black`,
   },
   hawayein: {
@@ -85,7 +75,6 @@ let songsObject = {
     singer: `Pritam`,
     songCover: `https://c.saavncdn.com/260/Hawayein-From-Jab-Harry-Met-Sejal--Hindi-201720170726112607-500x500.jpg`,
     songAudio: `assets/songs/hawayein.mp3`,
-    songImageALTText: `Cover image for the song Hawayein by Pritam`,
     themeColor: `#c65151,black`,
   },
   mereLiyeTumKaafiHo: {
@@ -93,7 +82,6 @@ let songsObject = {
     singer: `Ayushmann Khurrana`,
     songCover: `https://pagalnew.com/coverimages/Mere-Liye-Tum-Kaafi-Ho-Shubh-Mangal-Zyada-Saavdhan-500-500.jpg`,
     songAudio: `assets/songs/mereLiyeTumKaafiHo.mp3`,
-    songImageALTText: `Cover image for the song Mere Liye Tum Kaafi Ho by Ayushmann Khurrana`,
     themeColor: `#a4666d,black`,
   },
 };
